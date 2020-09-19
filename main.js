9ee198e4377c7ffbfe0236461c36926ae2430e11
grit <136907108@qq.com>
2020-09-20 00:19:48 +0800

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let webTable = JSON.parse(localStorage.getItem('hashTable'))
 let hashTable = webTable || [
-    { url: 'https://element.eleme.cn', ico: 'https://element.eleme.cn/favicon.ico' },
-    { url: 'https://jquery.cuishifeng.cn/submit.html', ico: 'https://jquery.cuishifeng.cn/favicon.ico' },
+    { name: 'elementUI', url: 'https://element.eleme.cn', ico: 'https://element.eleme.cn/favicon.ico' },
+    { name: 'jQueryAPI', url: 'https://jquery.cuishifeng.cn/submit.html', ico: 'https://jquery.cuishifeng.cn/favicon.ico' },
 ]
 const simplifyUrl = (url) => {
     url = url.replace('https://', '')
@@ -41,7 +41,7 @@ $('.add-web-site').on('click', (e) => {
     $('.web-input').css('display', 'block')
     $('.favorites').css('display', 'none')
     $('.mask').css('display', 'block')
-    $('.web-input-text').val('').focus()
+    $('.web-input-name').val('').focus()
 })
 
 function getWeb() {
@@ -51,7 +51,10 @@ function getWeb() {
       <li class="web-site">
       <a href="${simplifyUrl(res.url)}" target="_blank">
         <svg class="cancel" t="1600514363392" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3920" width="200" height="200"><path d="M726.016 665.984l-153.984-153.984 153.984-153.984-59.989333-59.989333-153.984 153.984-153.984-153.984-59.989333 59.989333 153.984 153.984-153.984 153.984 59.989333 59.989333 153.984-153.984 153.984 153.984zM512 86.016q176 0 301.013333 125.013333t125.013333 301.013333-125.013333 301.013333-301.013333 125.013333-301.013333-125.013333-125.013333-301.013333 125.013333-301.013333 301.013333-125.013333z" p-id="3921" fill="#d81e06"></path></svg>
+          <div class="web-idc">
           <img width="120px" class="web-ico" src="${res.ico}" alt="">
+          <span>${res.name}</span>
+      </div>
       </a>
       </li>
       `).insertBefore($('.add-web-site'))
@@ -69,16 +72,19 @@ $('.mask').click(() => {
     $('.mask').css('display', 'none')
     $('.favorites').css('display', 'block')
     $('.web-input-text').val('')
+    $('.web-input-name').val('')
 })
 
 
 $('.web-input-btn').on('click', addweb = () => {
+    let name = $('.web-input-name').val()
     let url = $('.web-input-text').val()
     if (url.indexOf('http') !== 0) {
         url = 'https://' + url
     }
     //console.log(simplifyUrl(url));
     hashTable.push({
+        name: name,
         url: simplifyUrl(url),
         ico: `${simplifyUrl(url)}/favicon.ico`
     })
